72ad3a95c2d665822bd973a9f5dc86fa9b435b75
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2020-05-18 16:55:16 -0400

    bringing in other data sources to BART

--- inst/bart_notes.R
+++ inst/bart_notes.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 library(repeldata)
+library(httr)
+library(jsonlite)
+
 devtools::load_all()
 conn <- repel_remote_conn()
 db_list_tables(conn)
@@ -9,7 +12,12 @@ library(dbarts)
 fit_nowcast_bart <- function(conn){
 
   traindat <- repel_cases_train(conn) %>%
-    select("country_iso3c", "report_year", "report_semester", "disease", "taxa")
+    select("country_iso3c", "report_year", "report_semester", "disease", "taxa") %>%
+    # filter out aquatic taxa?
+    filter(taxa %in% c("goats", "sheep",  "sheep/goats",
+                       "cattle", "birds", "camelidae", "dogs", "equidae",
+                       "cats", "cervidae" , "swine" , "buffaloes", "hares/rabbits"))
+  #TODO filter min year (3 semesters) by country?
 
   # generate lag values (using augument) for last 3 semesters -> mean imputation if missing
   traindat_augment <- nowcast_baseline_augment(conn = conn, newdata = traindat)
@@ -38,10 +46,39 @@ fit_nowcast_bart <- function(conn){
   traindat_augment <- left_join(traindat_augment, borders_sum)
 
   # measures of vet capacity - counts
+  vets <- tbl(conn, "annual_reports_veterinarians") %>%
+    collect() %>%
+    group_by(country_iso3c, report_year) %>%
+    summarize(veterinarian_count = sum(as.integer(total_count), na.rm = TRUE)) %>%
+    ungroup() %>%
+    mutate(report_year = as.integer(report_year))
 
-  # population of taxa (maybe from FAO?)
+  traindat_augment <- left_join(traindat_augment, vets)
 
-  # GDP (worldbank)
+  # taxa population
+  taxa <- tbl(conn, "annual_reports_animal_population") %>%
+    filter(population_units == "animals") %>%
+    collect() %>%
+    group_by(country_iso3c, report_year, taxa) %>%
+    summarize(taxa_pop_count = sum(as.integer(population_count), na.rm = TRUE)) %>%
+    ungroup() %>%
+    mutate(report_year = as.integer(report_year))
+
+  traindat_augment <- left_join(traindat_augment, taxa)
+
+  #fao <- read_csv("FAOSTAT_data_5-18-2020.csv") # alt to OIE counts - needs wrangling
+
+  # GDP
+  gdp <- fromJSON(paste0("http://api.worldbank.org/v2/country/all/indicator/NY.GDP.MKTP.CD?per_page=20000&format=json"))
+  gdp <- gdp[[2]] %>%
+    dplyr::select(report_year = date, gdp = value, country_iso3c = countryiso3code) %>%
+    as_tibble() %>%
+    filter(country_iso3c != "") %>%
+    drop_na() %>%
+    mutate(report_year = as.integer(report_year))
+
+  traindat_augment <- left_join(traindat_augment, gdp)
+
 
   # call dbart package - predict - return list structure with class  (class(obj), nowcast_bart, nowcast_model) (in this order)
   # list contains model object, other slots for anything else (eg metadata)
@@ -52,7 +89,7 @@ fit_nowcast_bart <- function(conn){
 
 }
 
- # model object saved as .rds
+# model object saved as .rds
 # drake? (later)
 # objects loaded in package (load with sys.file)? or plumber api (separate from this package)
 # tidymodels - strip down model objects with butcher?
