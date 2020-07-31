cd1383a32594ccc114bf07ab0e3322287b1fa4d8
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2020-07-31 15:07:54 -0400

    continue developing bart predict function

--- R/nowcast_predict.R
+++ R/nowcast_predict.R
@@ -4,15 +4,12 @@ repel_predict <- function(x, ...){
   UseMethod("repel_predict")
 }
 
-bart_predict <- function(model, newdata) {
-  apply(predict(model, newdata), 2, mean)
-}
-
 
 #' Predict from nowcast baseline model object
 #' @return list containing predicted count and whether disease is expected or not (T/F)
 #' @export
 #'
+#TODO rename augmented_data -> newdata
 repel_predict.nowcast_baseline <- function(model_object, augmented_data) {
   # this assumes NAs are 0s
   list(
@@ -27,24 +24,40 @@ repel_predict.nowcast_baseline <- function(model_object, augmented_data) {
 #' Predict from nowcast BART model object
 #' @return list containing predicted count and whether disease is expected or not (T/F)
 #' @importFrom here here
+#' @importFrom qs qread
 #' @export
 #'
-repel_predict.nowcast_bart <- function(model_object, augmented_data) {
+repel_predict.nowcast_bart <- function(model_object, newdata) {
 
-  bart_mod_disease_status <- read_rds(here::here("models/bart_mod_disease_status.rds"))
-  bart_mod_cases <- read_rds(here::here("models/bart_mod_cases.rds"))
-  modified_data_disease_status <- modify_augmented_data(augmented_data, "disease_status")
-  modified_data_cases <- modify_augmented_data(augmented_data, "cases")
+  # cases
+  bart_mod_cases <- qread(here::here("models/bart_mod_cases.qs"))
+  model <- bart_mod_cases[[1]]
+  model_data <- bart_mod_cases[[2]]
 
-  list(
-    predicted_cases = bart_predict(model = bart_mod_cases, newdata = modified_data_cases),
-    predicted_disease_status_probability = bart_predict(model = bart_mod_disease_status, newdata = modified_data_disease_status),
-    predicted_disease_status = round(predicted_disease_status_probability),
-    na_predicted_cases = which(is.na(predicted_cases)),
-    na_predicted_disease_status = which(is.na(predicted_disease_status))
-  )
+  newdata <- modify_augmented_data(newdata, "cases")
 
+  preds <- tibble(actual = model$y, predicted = model$yhat.test.mean)
+  preds <- preds %>%
+    mutate(predicted_adjust = ifelse(predicted < 0, 0, round(predicted)))  %>%
+    mutate(diff = abs(actual - predicted_adjust))
+  summary(preds$predicted)
+  summary(preds$actual)
+  summary(preds$predicted_adjust)
+  summary(preds$diff)
 
+  ## predicting on new data
+  levels(newdata$disease) <- levels(model_data$disease)
+  levels(newdata$taxa) <- levels(model_data$taxa)
+  levels(newdata$country_iso3c) <- levels(model_data$country_iso3c)
+  levels(newdata$disease_population) <- levels(model_data$disease_population)
 
+  pred <- predict(model, newdata)
+
+  dim(pred)
+  dim(newdata)
+  apply(pred, 2, mean) # all same number???
+
+  # binary
+  #apply(pnorm(model$yhat.train), 3, mean) # binary
 
 }
