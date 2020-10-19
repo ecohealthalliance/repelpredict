ae8018a0a45ef42043efa99ebc543a67d412666a
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2020-10-19 21:23:08 +0000

    archive bart model

--- R/nowcast_models.R
+++ R/nowcast_models.R
@@ -7,17 +7,6 @@ nowcast_baseline_model <- function(){
                  class = c("nowcast_baseline", "nowcast_model"))
 }
 
-
-#' Produces nowcast bart model object
-#' @import repeldata dplyr tidyr
-#' @return a list with description and classes
-#' @export
-nowcast_bart_model <- function(){
-  structure(list(description = "Nowcast bart model"),
-            class = c("nowcast_bart", "nowcast_tree", "nowcast_model"))
-}
-
-
 #' Produces nowcast gam model object
 #' @import repeldata dplyr tidyr
 #' @return a list with description and classes
