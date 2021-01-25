c5c8070cb33a50bbfc9f2174dfad3643a192772c
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2021-01-25 22:03:39 +0000

    network model object

---
+++ R/network_models.R
@@ -0,0 +1,9 @@
+#' Produces network lme model object
+#' @import repeldata dplyr tidyr
+#' @return a list with description and classes
+#' @export
+network_lme_model <- function(network_model = NULL){
+  structure(list(description = "Network LME model",
+                 network_model = network_model),
+            class = c("network_lme", "network_model"))
+}
