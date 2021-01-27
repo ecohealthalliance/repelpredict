8b4043dd530f5e3ce7ffe444f96cdde30a3317f6
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2021-01-27 20:40:14 +0000

    tweaks to data split

--- inst/network_generate_data_split_lookup.R
+++ inst/network_generate_data_split_lookup.R
@@ -5,19 +5,12 @@ set.seed(2222)
 
 model_object <-  network_lme_model()
 
-generate_indices <- function(n, prop){
-  init_sample <- sample(n, ceiling(prop*n), replace = FALSE)
-  init_sample_next <- ifelse(init_sample < n, init_sample + 1, init_sample - 1)
-  return(unique(sort(c(init_sample, init_sample_next))))
-}
-
 conn <- repeldata::repel_local_conn()
 #repeldata::repel_local_download()
 
 all_dat <- repel_init(model_object, conn) %>%
-  select(country_iso3c, disease, month) %>%
-  arrange() %>%
-  distinct()
+  distinct(country_iso3c, disease, month) %>%
+  arrange()
 
 # expand grid out to 2030, for every country
 all_dat_grid <- all_dat %>%
@@ -26,7 +19,7 @@ all_dat_grid <- all_dat %>%
               month = seq(min(all_dat$month), lubridate::ymd("2030-11-01"), by = "months")) %>%
   arrange(country_iso3c, disease, month) %>%
   group_by(country_iso3c, disease) %>%
-  mutate(validation_set = row_number() %in% generate_indices(n = n(), prop = 0.1))  %>%
+  mutate(validation_set = row_number() %in% sample(n(), ceiling(0.2*n()), replace = FALSE))  %>%
   ungroup()
 
 message(paste0("validation set is ", round(100*sum(all_dat_grid$validation_set)/nrow(all_dat_grid)), "% of data"))
