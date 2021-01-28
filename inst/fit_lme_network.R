a76be2f119b5d3b11c01bc424041de8c346cb9d9
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2021-01-28 22:15:26 +0000

    model fitting routine

--- inst/fit_lme_network.R
+++ inst/fit_lme_network.R
@@ -1,55 +1,76 @@
 devtools::load_all()
+library(lme4)
+
 # repeldata::repel_local_download()
 conn <- repeldata::repel_local_conn()
 model_object <- network_lme_model()
+
+
+scale2 <- function(x, na.rm = FALSE) (x - mean(x, na.rm = na.rm)) / sd(x, na.rm)
 
 # Fitting  ----------------------------------------------------------------
 # repel_init(model_object, conn)
 # repel_split(model_object, conn)
 # valdat <- repel_validation(model_object, conn)
 
-traindat <- repel_training(model_object, conn) %>%
-  select(country_iso3c, disease, month)
+# traindat <- repel_training(model_object, conn) %>%
+#   select(country_iso3c, disease, month)
+#
+# augmented_data <- repel_augment(model_object = model_object,
+#                                 conn = conn, newdata = traindat)
+# vroom::vroom_write(augmented_data, gzfile("tmp/network_augmented_data.csv.gz"))
 
-augmented_data <- repel_augment(model_object = model_object,
-                                conn = conn, newdata = traindat)
-vroom::vroom_write(augmented_data, gzfile("tmp/network_augmented_data.csv.gz"))
-
-augmented_data <- vroom::vroom("tmp/network_augmented_data.csv.gz")
+augmented_data <- vroom::vroom(here::here("tmp/network_augmented_data.csv.gz"))
 
 # model -------------------------------------------------------------------
-scale2 <- function(x, na.rm = FALSE) (x - mean(x, na.rm = na.rm)) / sd(x, na.rm)
-trade_names <- colnames(augmented_data)[str_detect(colnames(augmented_data), "trade_")]
 
+# V1
 augmented_data <- augmented_data %>%
   mutate(country_iso3c = as.factor(country_iso3c)) %>%
   mutate(disease = as.factor(disease)) %>%
-  mutate_at(trade_names, scale2)
+  mutate(outbreak_start = outbreak_start>0) %>%
+  select(country_iso3c, disease, month, outbreak_start,
+         shared_borders_from_outbreaks = shared_border,
+         ots_trade_dollars_from_outbreaks = ots_trade_dollars,
+         fao_livestock_heads_from_outbreaks = fao_livestock_heads) %>%
+  drop_na() %>%
+  mutate_if(is.numeric, scale2)
 
-# V1
-# frm <- as.formula(paste0("outbreak_start ~ ", paste(trade_names, collapse = " + "), " + (1 | country_iso3c) + (1 | disease)"))
-# mod <- lme4::glmer(data = augmented_data, family = binomial, formula = frm)
-# write_rds(mod, here::here("tmp/lme_mod_v1.rds"))
+vars <- c("shared_borders_from_outbreaks", "ots_trade_dollars_from_outbreaks", "fao_livestock_heads_from_outbreaks")
 
-# V2
-trade_names <- c("trade_asses", "trade_buffaloes", "trade_camels", "trade_cattle", "trade_chickens", "trade_ducks", "trade_goats",
-                 "trade_horses", "trade_mules", "trade_pigs", "trade_rabbits_and_hares", "trade_rodents_other", "trade_sheep", "trade_turkeys",
-                 "trade_meat_and_edible_meat_offal", "trade_dairy_produce", "trade_furskins_and_artificial_fur", "trade_raw_hides_and_skins_other_than_furskins_and_leather")
-augmented_data <- augmented_data %>%
-  select(country_iso3c, disease, month, outbreak_start, starts_with("n_outbreaks"), all_of(trade_names))
+# frm <- as.formula(paste0("outbreak_start ~ (disease | country_iso3c) + ",
+#                          paste0("(", vars, "|disease)", collapse = " + ")))
+# Error in (function (fr, X, reTrms, family, nAGQ = 1L, verbose = 0L, maxit = 100L,  :
+#                       pwrssUpdate did not converge in (maxit) iterations
+#                     Calls: <Anonymous> -> do.call -> <Anonymous>
 
-frm2 <- as.formula(paste0("outbreak_start ~ ", paste0(trade_names, " + (", trade_names, " | disease) + (", trade_names, "| country_iso3c)", collapse = " + ")))
+# frm <- as.formula(paste0("outbreak_start ~ ",
+#                          paste0("(", vars, "|disease) + (", vars, " | country_iso3c) ", collapse = " + ")))
+# ^ Error in (function (fr, X, reTrms, family, nAGQ = 1L, verbose = 0L, maxit = 100L,  :
+#                       (maxstephalfit) PIRLS step-halvings failed to reduce deviance in pwrssUpdate
+#                     Calls: <Anonymous> -> do.call -> <Anonymous>
+
+# frm <- as.formula(paste0("outbreak_start ~ ",
+#                          paste0("(", vars, "-1|disease) + (", vars, "-1|country_iso3c) ", collapse = " + ")))
+# Error in (function (fr, X, reTrms, family, nAGQ = 1L, verbose = 0L, maxit = 100L,  :
+#                       (maxstephalfit) PIRLS step-halvings failed to reduce deviance in pwrssUpdate
+#                     Calls: <Anonymous> -> do.call -> <Anonymous>
+
+frm <- as.formula(paste0("outbreak_start ~ ",
+                         paste0(vars, " + (", vars, "|disease) + (", vars, "|country_iso3c) ", collapse = " + ")))
 
 all_cores <- parallel::detectCores(logical = FALSE)
 cl <- parallel::makePSOCKcluster(all_cores)
 doParallel::registerDoParallel(cl)
 
-mod2 <- lme4::glmer(data = augmented_data, family = binomial, formula = frm2)
-write_rds(mod2, here::here("tmp/lme_mod_v2.rds"))
+mod <- lme4::glmer(data = augmented_data, family = binomial, formula = frm,
+                   control = glmerControl(
+                     optimizer="bobyqa",
+                     check.nobs.vs.nlev="ignore",
+                     check.nobs.vs.nRE="ignore"))
+write_rds(mod, here::here("tmp/lme_mod_v1.rds"))
 
 parallel::stopCluster(cl = cl)
 
-# outbreak_start ~ (disease | country_iso3c) + (trade_var1|disease)
-
 # disconnect --------------------------------------------------------------
-DBI::dbDisconnect(conn)
+# DBI::dbDisconnect(conn)
