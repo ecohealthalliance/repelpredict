93b5ec05f54c86c4dddba2fe137f47bb3ad76925
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2021-05-17 22:02:51 +0000

    more annotation

--- R/data_split.R
+++ R/data_split.R
@@ -32,7 +32,7 @@ repel_split.nowcast_model <- function(model_object, conn, clean_disease_names = 
     arrange(country_iso3c, taxa, disease, disease_population, report_year, report_semester) %>%
     left_join(validation_split,  by = c("country_iso3c", "report_year", "report_semester", "taxa", "disease", "disease_population"))
 
-  assert_that(!any(is.na(all_dat$validation_set)))
+  assert_that(!any(is.na(all_dat$validation_set)))  # if this fails, rerun inst/nowcast_generate_data_split_lookup.R
 
   if(clean_disease_names){
     # from inst/network_generate_disease_lookup.R
@@ -44,7 +44,7 @@ repel_split.nowcast_model <- function(model_object, conn, clean_disease_names = 
       left_join(diseases_recode, by = "disease") %>%
       select(-disease) %>%
       rename(disease = disease_recode)
-    assertthat::assert_that(!any(is.na(unique(all_dat$disease))))
+    assertthat::assert_that(!any(is.na(unique(all_dat$disease)))) # if this fails, rerun inst/nowcast_generate_disease_lookup.R
   }
 
   return(all_dat)
