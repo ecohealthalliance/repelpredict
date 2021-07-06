ef35feb98659983f4133acedb188118816f7b798
emmamendelsohn <emma.mendelsohn@gmail.com>
2021-07-06 15:21:27 -0400

    country origin plots

--- R/get_network_variable_importance.R
+++ R/get_network_variable_importance.R
@@ -1,5 +1,5 @@
 #' Get variable importance for given disease and month, by country
-#' @param network_lme_augment_predict output of `get_cached_network_predictions()`
+#' @param network_augment_predict output of `get_cached_network_predictions()`
 #' @param randef output of `get_cached_network_random_effects()`
 #' @param country_iso3c iso3c(s) of countries
 #' @param disease disease(s) name(s)
@@ -7,13 +7,13 @@
 #' @return dataframe containing actual values ('x'), coefficients ('coef') and variable importance
 #' @import dplyr tidyr
 #' @export
-get_network_variable_importance <- function(network_lme_augment_predict, randef,
+get_network_variable_importance <- function(network_augment_predict, randef,
                                             country_iso3c, diseases = get_oie_high_importance_diseases(),
                                             month = NULL){
 
-  if(is.null(month)) month <- max(network_lme_augment_predict$month)
+  if(is.null(month)) month <- max(network_augment_predict$month)
 
-  network_lme_augment_predict %>%
+  network_augment_predict %>%
     filter(disease %in% !!diseases)  %>%
     filter(country_iso3c %in% !!country_iso3c) %>%
     filter(month == !!month) %>%
@@ -27,3 +27,49 @@ get_network_variable_importance <- function(network_lme_augment_predict, randef,
 
 }
 
+#' Get variable importance for given disease and month, by country
+#' @param disagg_network_augment_predict output of `get_cached_disagg_network_predictions()`
+#' @param randef output of `get_cached_network_random_effects()`
+#' @param country_iso3c iso3c(s) of countries
+#' @param disease disease(s) name(s)
+#' @param month in format of "yyyy-mm-01". if NULL, uses latest month in dataset
+#' @return dataframe containing actual values ('x'), coefficients ('coef') and variable importance
+#' @import dplyr tidyr
+#' @export
+get_network_variable_importance_with_origins <- function(disagg_network_augment_predict, randef,
+                                            country_iso3c, diseases = get_oie_high_importance_diseases(),
+                                            month = NULL){
+
+  if(is.null(month)) month <- max(network_lme_augment_predict$month)
+
+  disagg_network_augment_predict <- disagg_network_augment_predict %>%
+    rename(outbreak_ongoing = outbreak_subsequent_month) %>%
+    rename(country_origin_iso3c = country_origin)
+
+  country_lookup <- tibble(country_iso3c = unique(disagg_network_augment_predict$country_iso3c)) %>%
+    mutate(country = countrycode::countrycode(country_iso3c, origin = "iso3c", destination = "country.name"))
+
+  country_lookup2 <- tibble(country_origin_iso3c = unique(disagg_network_augment_predict$country_origin_iso3c)) %>%
+    mutate(country_origin = countrycode::countrycode(country_origin_iso3c, origin = "iso3c", destination = "country.name"))
+
+  disease_lookup <- tibble(disease = unique(disagg_network_augment_predict$disease)) %>%
+    mutate(disease_clean = str_to_title(str_replace_all(disease, "_", " ")))
+
+  disagg_network_augment_predict <- left_join(disagg_network_augment_predict, country_lookup) %>%
+    left_join(country_lookup2) %>%
+    left_join(disease_lookup) %>%
+    as_tibble()
+
+  disagg_network_augment_predict %>%
+    filter(country_iso3c == "USA",
+           disease %in% get_oie_high_importance_diseases(),
+           month == "2018-12-01") %>%
+    pivot_wider(names_from = continent, values_from = continent, names_prefix = "continent") %>%
+    mutate_at(vars(starts_with("continent")), ~ifelse(!is.na(.), 1, NA)) %>%
+    pivot_longer(cols = -c("country_iso3c", "country", "country_origin_iso3c", "country_origin", "disease", "disease_clean", "month", "outbreak_start","endemic", "outbreak_ongoing"), names_to = "variable", values_to = "x") %>%
+    drop_na(x) %>%
+    left_join(randef) %>%
+    mutate(variable_importance = x * coef) %>%
+    mutate(pos = variable_importance > 0)
+
+}
--- inst/reports/repel-model-summary.md
+++ inst/reports/repel-model-summary.md
@@ -127,5 +127,55 @@ output:
 </details>
 
 
+### Variable importance by disease and country (USA), with country origins
+
+
+
+
+<details>
+<summary>African Swine Fever</summary>
+![](repel-model-summary_files/figure-html/vi-by-disease-country-origin-plots-1-1.png)<!-- -->
+</details>
+<details>
+<summary>Classical Swine Fever</summary>
+![](repel-model-summary_files/figure-html/vi-by-disease-country-origin-plots-2-1.png)<!-- -->
+</details>
+<details>
+<summary>Foot And Mouth Disease</summary>
+![](repel-model-summary_files/figure-html/vi-by-disease-country-origin-plots-3-1.png)<!-- -->
+</details>
+<details>
+<summary>Highly Pathogenic Avian Influenza</summary>
+![](repel-model-summary_files/figure-html/vi-by-disease-country-origin-plots-4-1.png)<!-- -->
+</details>
+<details>
+<summary>Lumpy Skin Disease</summary>
+![](repel-model-summary_files/figure-html/vi-by-disease-country-origin-plots-5-1.png)<!-- -->
+</details>
+<details>
+<summary>Newcastle Disease</summary>
+![](repel-model-summary_files/figure-html/vi-by-disease-country-origin-plots-6-1.png)<!-- -->
+</details>
+<details>
+<summary>Ovine Bluetongue Disease</summary>
+![](repel-model-summary_files/figure-html/vi-by-disease-country-origin-plots-7-1.png)<!-- -->
+</details>
+<details>
+<summary>Ovine Pox Disease</summary>
+![](repel-model-summary_files/figure-html/vi-by-disease-country-origin-plots-8-1.png)<!-- -->
+</details>
+<details>
+<summary>Peste Des Petits Ruminants</summary>
+![](repel-model-summary_files/figure-html/vi-by-disease-country-origin-plots-9-1.png)<!-- -->
+</details>
+<details>
+<summary>Pleuropneumonia</summary>
+![](repel-model-summary_files/figure-html/vi-by-disease-country-origin-plots-10-1.png)<!-- -->
+</details>
+<details>
+<summary>Rift Valley Fever</summary>
+![](repel-model-summary_files/figure-html/vi-by-disease-country-origin-plots-11-1.png)<!-- -->
+</details>
+
 ### Disease status summary
 
