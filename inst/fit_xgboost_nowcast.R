8d716fa048d63af0437b05f1a2db69f27898b177
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2021-10-10 15:46:53 -0400

    cleaning up inst fit nowcast script, looking at predictions

--- inst/fit_xgboost_nowcast.R
+++ inst/fit_xgboost_nowcast.R
@@ -5,12 +5,16 @@ conn <- repeldata::repel_remote_conn()
 model_object <-  nowcast_boost_model(disease_status_model = NULL, cases_model = NULL)
 
 # Preprocess and augment -----------------------------------------------------------------
-
 # for model fitting
-# valdat <- repel_validation(model_object, conn)
-# traindat <- repel_training(model_object, conn)
-# map(traindat, ~any(is.na(.)))
-# write_rds(traindat, here::here("tmp/nowcast_traindat.rds"))
+
+valdat <- repel_validation(model_object, conn)
+map(valdat, ~any(is.na(.))) # NA in cases expected
+write_rds(valdat, here::here("tmp/nowcast_valdat.rds"))
+valdat <- read_rds(here::here("tmp/nowcast_valdat.rds"))
+
+traindat <- repel_training(model_object, conn)
+map(traindat, ~any(is.na(.))) # NA in cases expected
+write_rds(traindat, here::here("tmp/nowcast_traindat.rds"))
 traindat <- read_rds(here::here("tmp/nowcast_traindat.rds"))
 
 augmented_data <- repel_augment(model_object = model_object,
@@ -19,58 +23,67 @@ augmented_data <- repel_augment(model_object = model_object,
   arrange(country_iso3c, disease, taxa, report_year, report_semester)
 assertthat::are_equal(nrow(janitor::get_dupes(augmented_data, all_of(grouping_vars))), 0)
 
-# test = map_lgl(augmented_data, ~any(is.na(.)))
-# test[test]
+augmented_data_na_check = map_lgl(augmented_data, ~any(is.na(.)))
+augmented_data_na_check[augmented_data_na_check] # NAs expected for cases, disease_status_lagx_unreported
 
-# write_rds(augmented_data, "tmp/augmented_data.rds")
-# augmented_data <- read_rds("tmp/augmented_data.rds")
+write_rds(augmented_data, "tmp/augmented_data.rds")
+augmented_data <- read_rds("tmp/augmented_data.rds")
 
 # Fitting -----------------------------------------------------------------
 # fitting takes about a day for these two models on prospero
+
 repel_fit(model_object =  model_object,
           augmented_data = augmented_data,
           model = "disease_status",
           output_directory = "models")
 
-
-# train_dat <- model_object$cases_model$pre$actions$recipe$recipe$template
-# train_dat_predict <- predict(object = model_object$cases_model, new_data = train_dat)
-# hist(train_dat_predict$.pred)
-
-# try running with other parallel approach
-# then look into status model
-# do full run on repelinfra
-
-
+# takes ~1 hr
 repel_fit(model_object = model_object,
           augmented_data = augmented_data,
           model = "cases",
           output_directory = "models")
 
-# Predict/Forecast example --------------------------------------------------------
-  model_object <-  nowcast_boost_model(
-    disease_status_model = aws.s3::s3readRDS(bucket = "repeldb/models", object = "boost_mod_disease_status.rds"),
-    cases_model = aws.s3::s3readRDS(bucket = "repeldb/models", object = "boost_mod_cases.rds")
-  )
+# Look at cases predictions --------------------------------------------
+model_object <-  nowcast_boost_model(
+  disease_status_model = NULL, # for now looking only at cases
+  # disease_status_model = aws.s3::s3readRDS(bucket = "repeldb/models", object = "boost_mod_disease_status.rds"),
+  cases_model = aws.s3::s3readRDS(bucket = "repeldb/models", object = "boost_mod_cases.rds")
+)
 
-# full expanded dataset
-augmented_data <- repel_augment(model_object = model_object,
+# predict on augmented traindat from the model object (filtered for positive cases in current or previous semester)
+augmented_data_from_model <- model_object$cases_model$pre$actions$recipe$recipe$template
+dim(augmented_data_from_model)
+augmented_data_from_model_predict <- predict(object = model_object$cases_model, new_data = augmented_data_from_model)
+hist(augmented_data_from_model_predict$.pred)
+actual <- model_object$cases_model$pre$mold$outcomes$cases
+predicted <- augmented_data_from_model_predict$.pred
+plot(actual, predicted)
+
+# predict on augmented traindat (not filtered for positives)
+dim(augmented_data)
+augmented_data_predict <- predict(object = model_object$cases_model, new_data = augmented_data)
+hist(augmented_data_predict$.pred)
+actual <- augmented_data$cases
+predicted <- augmented_data_predict$.pred
+plot(log10(actual), predicted)
+
+# full expanded dataset (not traindat)
+augmented_data_expanded <- repel_augment(model_object = model_object,
                                 conn = conn,
                                 subset = NULL) %>%
   arrange(country_iso3c, disease, taxa, report_year, report_semester)
+write_rds(augmented_data_expanded, "tmp/augmented_data_expanded.rds")
+augmented_data_expanded <- read_rds("tmp/augmented_data_expanded.rds")
 
-# test = map_lgl(augmented_data, ~any(is.na(.)))
-# test[test]
-augmented_data_predict <- predict(object = model_object$cases_model, new_data = augmented_data)
-hist(augmented_data_predict$.pred)
+augmented_data_expanded_na_check = map_lgl(augmented_data_expanded, ~any(is.na(.)))
+augmented_data_expanded_na_check[augmented_data_expanded_na_check] # NAs expected for cases, disease_status_lagx_unreported
 
-#map(disease_status_model$pre$actions$recipe$recipe$template, ~any(is.na(.))) # NA only in removed fields
-
-predict_xgboost <-repel_predict(model_object = model_object,
-                                    newdata = augmented_data)
-forecasted_xgboost <-repel_forecast(model_object = model_object,
-                                    conn = conn,
-                                    subset = traindat)
+dim(augmented_data_expanded)
+augmented_data_expanded_predict <- predict(object = model_object$cases_model, new_data = augmented_data_expanded)
+hist(augmented_data_expanded_predict$.pred)
+actual <- augmented_data_expanded$cases
+predicted <- augmented_data_expanded_predict$.pred
+plot(log10(actual), predicted)
 
 # Baseline ----------------------------------------------------------------
 model_object <-  nowcast_baseline_model()
